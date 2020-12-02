93f189c1481acbe38f637918139900523aeb4ade
Ksenia <72394648+Solnyshkoo@users.noreply.github.com>
2020-12-02 22:26:56 +0300

    Add files via upload

---
+++ Sem9_10/Task1/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Seminar_2_09_10
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            button2.Visible = false;
+            button2.Enabled = true;
+            button1.Visible = true;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            button1.Visible = false;
+            button1.Enabled = true;
+            button2.Visible = true;
+        }
+    }
+}
